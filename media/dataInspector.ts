1ee006f7791a5c26a7a71643134d5db673c92cac
Logan Ramos <lramos15@gmail.com>
2020-06-11 13:32:11 -0400

    Remove invalid from the wording in data inspector

--- media/dataInspector.ts
+++ media/dataInspector.ts
@@ -8,19 +8,28 @@ import { ByteData } from "./byteData";
  */
 export function clearDataInspector(): void {
     // This function only gets called when these elements exist so these casts are safe
-	(document.getElementById("binary8") as HTMLInputElement).value = "Invalid";
+	(document.getElementById("binary8") as HTMLInputElement).value = "";
+	(document.getElementById("binary8") as HTMLInputElement).disabled = true;
 	for (let i = 0; i < 4; i++) {
 		const numBits = (i + 1) * 8;
-		(document.getElementById(`int${numBits}`) as HTMLInputElement).value = "Invalid";
+		(document.getElementById(`int${numBits}`) as HTMLInputElement).disabled = true;
+		(document.getElementById(`int${numBits}`) as HTMLInputElement).value = "";
 		
-		(document.getElementById(`uint${numBits}`) as HTMLInputElement).value = "Invalid";
+		(document.getElementById(`uint${numBits}`) as HTMLInputElement).disabled = true;
+		(document.getElementById(`uint${numBits}`) as HTMLInputElement).value = "";
 	}
-	(document.getElementById("int64") as HTMLInputElement).value = "Invalid";
-	(document.getElementById("uint64") as HTMLInputElement).value = "Invalid";
-	(document.getElementById("utf8") as HTMLInputElement).value = "Invalid";
-	(document.getElementById("utf16") as HTMLInputElement).value = "Invalid";
-	(document.getElementById("float32") as HTMLInputElement).value = "Invalid";
-	(document.getElementById("float64") as HTMLInputElement).value = "Invalid";
+	(document.getElementById("int64") as HTMLInputElement).value = "";
+	(document.getElementById("int64") as HTMLInputElement).disabled = true;
+	(document.getElementById("uint64") as HTMLInputElement).value = "";
+	(document.getElementById("uint64") as HTMLInputElement).disabled = true;
+	(document.getElementById("utf8") as HTMLInputElement).value = "";
+	(document.getElementById("utf8") as HTMLInputElement).disabled = true;
+	(document.getElementById("utf16") as HTMLInputElement).value = "";
+	(document.getElementById("utf16") as HTMLInputElement).disabled = true;
+	(document.getElementById("float32") as HTMLInputElement).value = "";
+	(document.getElementById("float32") as HTMLInputElement).disabled = true;
+	(document.getElementById("float64") as HTMLInputElement).value = "";
+	(document.getElementById("float64") as HTMLInputElement).disabled = true;
 }
 
 /**
@@ -30,26 +39,35 @@ export function clearDataInspector(): void {
  */
 export function populateDataInspector(byte_obj: ByteData, littleEndian: boolean): void {
 	(document.getElementById("binary8") as HTMLInputElement).value = byte_obj.toBinary();
+	(document.getElementById("binary8") as HTMLInputElement).disabled = false;
 	for (let i = 0; i < 4; i++) {
 		const numBits = (i + 1) * 8;
 		const signed = byte_obj.byteConverter(numBits, true, littleEndian);
 		const unsigned = byte_obj.byteConverter(numBits, false, littleEndian);
 	
 		(document.getElementById(`int${numBits}`) as HTMLInputElement).value = isNaN(Number(signed)) ? "End of File" : signed.toString();
+		(document.getElementById(`int${numBits}`) as HTMLInputElement).disabled = false;
 		(document.getElementById(`uint${numBits}`) as HTMLInputElement).value = isNaN(Number(unsigned)) ? "End of File" : unsigned.toString();
+		(document.getElementById(`uint${numBits}`) as HTMLInputElement).disabled = false;
 		if (numBits === 16) {
 			(document.getElementById("utf16") as HTMLInputElement).value = isNaN(Number(unsigned)) ? "End of File" : String.fromCharCode(Number(unsigned));
+			(document.getElementById("utf16") as HTMLInputElement).disabled = false;
 		} else if (numBits === 32) {
 			// The boolean for signed doesn't matter for floats so this could also be 32, false, littleEndian, true
 			const float32 = byte_obj.byteConverter(32, true, littleEndian, true);
 			(document.getElementById("float32") as HTMLInputElement).value = isNaN(Number(float32)) ? "End of File" : float32.toString();
+			(document.getElementById("float32") as HTMLInputElement).disabled = false;
 		}
 	}
 	const signed64 = byte_obj.byteConverter(64, true, littleEndian);
 	const unsigned64 = byte_obj.byteConverter(64, false, littleEndian);
 	(document.getElementById("int64") as HTMLInputElement).value = isNaN(Number(signed64)) ? "End of File" : signed64.toString();
+	(document.getElementById("int64") as HTMLInputElement).disabled = false;
 	(document.getElementById("uint64") as HTMLInputElement).value = isNaN(Number(unsigned64)) ? "End of File" : unsigned64.toString();
+	(document.getElementById("uint64") as HTMLInputElement).disabled = false;
 	(document.getElementById("utf8") as HTMLInputElement).value = String.fromCharCode(byte_obj.to8bitUInt());
+	(document.getElementById("utf8") as HTMLInputElement).disabled = false;
 	const float64 = byte_obj.byteConverter(64, true, littleEndian, true);
 	(document.getElementById("float64") as HTMLInputElement).value = isNaN(Number(float64)) ? "End of File" : float64.toString();
+	(document.getElementById("float64") as HTMLInputElement).disabled = false;
 }
