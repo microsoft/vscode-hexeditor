0f737c616791f5a35088f643a97955c165077cf7
Logan Ramos <lramos15@gmail.com>
2020-06-11 09:45:57 -0400

    Add open with hex editor command (#37)
    * Add open with hex editor command
    
    * Bump version for recovery build

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import * as vscode from "vscode";
-import TelemetryReporter   from "vscode-extension-telemetry";
+import TelemetryReporter from "vscode-extension-telemetry";
 const { name, version, aiKey } = require("./../../package.json") as {name: string; version: string; aiKey: string};
 import { HexEditorProvider } from "./hexEditorProvider";
 
@@ -14,6 +14,10 @@ let telemetryReporter: TelemetryReporter;
 export function activate(context: vscode.ExtensionContext): void {
 	console.log("Hexeditor is active!");
 	telemetryReporter = new TelemetryReporter(extensionID, version, aiKey);
+	const openWithCommand = vscode.commands.registerTextEditorCommand("hexEditor.openFile", (textEditor: vscode.TextEditor) => {
+		vscode.commands.executeCommand("vscode.openWith", textEditor.document.uri, "hexEditor.hexedit");
+	});
+	context.subscriptions.push(openWithCommand);
 	context.subscriptions.push(telemetryReporter);
 	context.subscriptions.push(HexEditorProvider.register(context, telemetryReporter));
 }
