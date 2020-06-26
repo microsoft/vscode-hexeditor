e52742c7fd5ef054a7d8d59f0b72e5f2f723fd30
Logan Ramos <lramos15@gmail.com>
2020-06-26 11:46:04 -0400

    JS Docs for edit handler

--- media/editHandler.ts
+++ media/editHandler.ts
@@ -20,6 +20,9 @@ export interface EditMessage {
     readonly sameOnDisk: boolean;
 }
 
+/**
+ * @description Class responsible for handling edits within the virtual document
+ */
 export class EditHandler {
     private pendingEdit: DocumentEdit | undefined;
 
@@ -27,6 +30,12 @@ export class EditHandler {
         this.pendingEdit = undefined;
     }
 
+    /**
+     * @description Handles when a user starts typing on a hex element
+     * @param {HTMLSpanElement} element The element which the keypress was fired on
+     * @param {string} keyPressed The key which was pressed
+     * @param {number} keyCode The keycode for that key 
+     */
     public async editHex(element: HTMLSpanElement, keyPressed: string, keyCode: number): Promise<void> {
         // If it's not a valid hex input or delete we ignore it
         if (!((keyCode >= 65 && keyCode <= 70) || (keyCode >= 48 && keyCode <= 57) || keyCode === 8)) return;
@@ -69,6 +78,11 @@ export class EditHandler {
         }
     }
 
+    /**
+     * @description Handles when the user starts typing on an ascii element
+     * @param {HTMLSpanElement} element The element which the keystroke was fired on 
+     * @param {string} keyPressed The key which was pressed
+     */
     public async editAscii(element: HTMLSpanElement, keyPressed: string): Promise<void> {
         // We don't want to do anything if the user presses a key such as home etc which will register as greater than 1 char
         if (keyPressed.length != 1) return;
@@ -96,6 +110,11 @@ export class EditHandler {
         this.pendingEdit = undefined;
     }
 
+    /**
+     * @description Given a hex value updates the respective ascii value
+     * @param {string | undefined} hexValue The hex value to convert to ascii 
+     * @param {number} offset The offset of the ascii value to update 
+     */
     private updateAscii(hexValue: string | undefined, offset: number): void {
         // For now if it's undefined we will just ignore it, but this would be the delete case
         if (!hexValue) return;
@@ -106,6 +125,11 @@ export class EditHandler {
         ascii.classList.add("edited");
     }
 
+    /**
+     * @description Given an ascii value updates the respective hex value
+     * @param {string} asciiValue The ascii value to convert to hex 
+     * @param {number} offset The offset of the hex value to update 
+     */
     private updateHex(asciiValue: string, offset: number): void {
         // The way the DOM is constructed the hex element will always be the first one
         const hex = getElementsWithGivenOffset(offset)[0];
@@ -114,6 +138,9 @@ export class EditHandler {
         hex.classList.add("edited");
     }
 
+    /**
+     * @description Completes the current edit, this is used if the user navigates off the cell and it wasn't done being edited
+     */
     public async completePendingEdits(): Promise<void> {
         if (this.pendingEdit && this.pendingEdit.element && this.pendingEdit.newValue) {
             // We don't want to stop the edit if it is selected as that can mean the user will be making further edits
@@ -138,6 +165,10 @@ export class EditHandler {
         }
     }
 
+    /**
+     * @description Given an edit sends it to the exthost so that the ext host and webview are in sync
+     * @param {DocumentEdit} edit The edit to send to the exthost 
+     */
     private async sendEditToExtHost(edit: DocumentEdit): Promise<void> {
         // The ext host only accepts 8bit unsigned ints, so we must convert the edits back into that representation
         const oldValue = edit.previousValue ? parseInt(edit.previousValue, 16) : undefined;
@@ -159,6 +190,10 @@ export class EditHandler {
 
     }
 
+    /**
+     * @description Given a list of edits undoes them from the document
+     * @param {EditMessage[]} edits The list of edits to undo 
+     */
     public undo(edits: EditMessage[]): void {
         for (const edit of edits) {
             // This would be the delete case, but for now we will leave it alone
@@ -182,6 +217,10 @@ export class EditHandler {
         }
     }
 
+    /**
+     * @description Given a list of edits reapplies them to the document
+     * @param {EditMessage[]} edits The list of edits to redo 
+     */
     public redo(edits: EditMessage[]): void {
         for (const edit of edits) {
             if (edit.newValue === undefined) return;
