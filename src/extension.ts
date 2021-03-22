7707581d5a3401f385f7325c39a00e3d18c4d611
Logan Ramos <lramos15@gmail.com>
2021-03-22 10:46:43 -0400

    Fix incorrect extension id

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,28 @@
 import * as vscode from "vscode";
 import TelemetryReporter from "vscode-extension-telemetry";
 import { DataInspectorView } from "./dataInspectorView";
-const { name, version, aiKey } = require("./../../package.json") as { name: string; version: string; aiKey: string };
 import { HexEditorProvider } from "./hexEditorProvider";
 import { openOffsetInput } from "./util";
 
 // Telemetry information
-const extensionID = `vscode-${name}`;
+const extensionID = "ms-vscode.hexeditor";
 
 let telemetryReporter: TelemetryReporter;
+
+function readConfigFromPackageJson(extensionID: string): { version: string; aiKey: string } {
+	const packageJSON = vscode.extensions.getExtension(extensionID)!.packageJSON;
+	return {
+		version: packageJSON.version,
+		aiKey: packageJSON.aiKey
+	};
+}
 
 export function activate(context: vscode.ExtensionContext): void {
 	// Register the data inspector as a separate view on the side
 	const dataInspectorProvider = new DataInspectorView(context.extensionUri);
+	const configValues = readConfigFromPackageJson(extensionID);
 	context.subscriptions.push(vscode.window.registerWebviewViewProvider(DataInspectorView.viewType, dataInspectorProvider));
-	telemetryReporter = new TelemetryReporter(extensionID, version, aiKey);
+	telemetryReporter = new TelemetryReporter(extensionID, configValues.version, configValues.aiKey);
 	const openWithCommand = vscode.commands.registerTextEditorCommand("hexEditor.openFile", (textEditor: vscode.TextEditor) => {
 		vscode.commands.executeCommand("vscode.openWith", textEditor.document.uri, "hexEditor.hexedit");
 	});
